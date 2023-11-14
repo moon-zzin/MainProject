29a70179b104aede778bad18023f408bbedb1c84
moon-zzin <yanggo10909@naver.com>
2023-11-14 15:00:10 +0900

    11-14/moon+kim_4

--- app/src/main/java/com/example/mainproject/MainActivity.kt
+++ app/src/main/java/com/example/mainproject/MainActivity.kt
@@ -7,8 +7,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mainproject.databinding.ActivityMainBinding
-//메인
-
+//
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
