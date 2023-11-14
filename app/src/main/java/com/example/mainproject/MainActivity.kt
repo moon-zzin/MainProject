419814117e48d94339a5c70d9efcd6587daf3ea1
moon-zzin <yanggo10909@naver.com>
2023-11-14 14:59:29 +0900

    11-14/moon+kim_3

--- app/src/main/java/com/example/mainproject/MainActivity.kt
+++ app/src/main/java/com/example/mainproject/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mainproject.databinding.ActivityMainBinding
+//메인
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
